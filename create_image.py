564f3cb1cbb73c5eab5bfd5101f179462a962494
KevinF1 <kevin.joel.fernandes@gmail.om>
2020-04-29 23:51:03 +0200

    Modifying relevant elements

--- create_image.py
+++ create_image.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_image(series, x_values, x_size, y_size, save_file=None):
-    fig, ax  = plt.subplots(1, 1, figsize=(x_size, y_size))
-    ax.plot(series)
-    ax.set_xticklabels(x_values)
+
+def plot_image(ordonne_y, abscisse_x, x_size, y_size, save_file=None):
+    fig, ax = plt.subplots(1, 1, figsize=(x_size, y_size))
+    ax.plot(ordonne_y)
+    ax.set_xticklabels(abscisse_x)
     if save_file is not None:
         plt.savefig(save_file)
     else:
@@ -12,6 +13,6 @@ def plot_image(series, x_values, x_size, y_size, save_file=None):
 
 
 if __name__ == '__main__':
-    x_values = np.linspace(0,100)/10
+    x_values = np.linspace(0, 100)/10
     series = np.sin(x_values)
     plot_image(series, x_values, 15, 10)
