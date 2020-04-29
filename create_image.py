910565e2b0dc620e47e34eb11c4e9f3df1673b7c
pierresdr <robustepierre@users.noreply.github.com>
2020-04-29 23:25:44 +0200

    added main

--- create_image.py
+++ create_image.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_image(series, x_values, x_size, y_size, save_file=None):
     fig, ax  = plt.subplots(1, 1, figsize=(x_size, y_size))
     ax.plot(series)
-    ax.set_xticks(x_values)
-    ax.set_xlabel(x_values)
+    ax.set_xticklabels(x_values)
     if save_file is not None:
         plt.savefig(save_file)
     else:
         plt.show()
+
+
+if __name__ == '__main__':
+    x_values = np.linspace(0,100)/10
+    series = np.sin(x_values)
+    plot_image(series, x_values, 15, 10)
