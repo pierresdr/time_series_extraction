ebe876b7a77f31326615c740c42b5c01eacddcb4
pierresdr <robustepierre@users.noreply.github.com>
2020-05-17 16:42:41 +0200

    exctract plot

---
+++ time_serie_extraction/extract_plot.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import random
+import numpy as np
+import cv2
+
+
+def extract_x_axis(plot):
+    try:
+        plot = cv2.imread(plot)
+    except:
+        pass
+    
+        
+
+def extract_y_axis(plot):
+    pass
+
+
+if __name__ == '__main__':
+    extract_x_axis()
